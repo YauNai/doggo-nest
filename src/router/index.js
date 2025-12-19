346a14c42eaba3273ea13570f3df2831962bb0ad
YauNai <w62796854w@gmail.com>
2025-12-19 15:13:23 +0800

    Add not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AdminView from '@/views/AdminView.vue'
 import { useAuth } from '@/composables/useAuth'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -16,6 +18,11 @@ const router = createRouter({
       name: 'admin',
       component: AdminView,
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFoundView,
+    }
   ],
 })
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,38 @@
+<template>
+  <section id="not-found">
+    <img :src="imgNotFound" alt="NotFound" class="rounded-4">
+    <h1>404 - 哎呀！</h1>
+    <h2>這個頁面不存在！</h2>
+    <RouterLink to="/" class="btn btn-lg">返回首頁</RouterLink>
+  </section>
+</template>
+
+<script setup>
+  import imgNotFound from '../assets/images/NotFound.jpg'
+</script>
+
+<style scoped>
+  #not-found {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #not-found>* {
+    margin: 20px;
+  }
+
+  img {
+    width: 500px;
+    box-shadow: 0 0 10px var(--brand-dark);
+    transition: all 0.3s ease;
+  }
+
+  img:hover {
+    box-shadow: 0px 0 30px var(--brand-dark);
+    transform: scale(1.01);
+  }
+
+</style>
